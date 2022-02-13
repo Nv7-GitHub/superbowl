24fcec7f79145acf71e46083102067cca87a68f6
Nv7 <junk4Nv7@gmail.com>
2022-02-13 10:20:58 -0800

    Add fetch code

--- data.go
+++ data.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -24,8 +25,16 @@ var dataFile *os.File
 var teams map[string]Team
 
 func init() {
-	var err error
-	dataFile, err = os.Open("data.json")
+	// Check if exists
+	_, err := os.Stat("data.json")
+	if os.IsNotExist(err) {
+		err = os.WriteFile("data.json", []byte("{}"), os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	dataFile, err = os.OpenFile("data.json", os.O_RDWR, 0)
 	if err != nil {
 		panic(err)
 	}
@@ -44,6 +53,7 @@ func GetTeam(name string) Team {
 	}
 
 	// Fetch team
+	fmt.Println("Fetching: ", name)
 	resp, err := http.Get("https://www.espn.com/nfl/team/schedule/_/name/" + name) // lar/los_angeles_rams for LA Rams
 	if err != nil {
 		panic(err)
@@ -65,8 +75,55 @@ func GetTeam(name string) Team {
 	}
 
 	// Parse JSON
-	schedule := data["page"].(map[string]interface{})["content"].(map[string]interface{})["scheduleData"].(map[string]interface{})["teamSchedule"].([]interface{})
-	fmt.Println(schedule)
+	events := data["page"].(map[string]interface{})["content"].(map[string]interface{})["scheduleData"].(map[string]interface{})["teamSchedule"].([]interface{})
+	games := make([]Game, 0)
+	for _, ev := range events {
+		event := ev.(map[string]interface{})["events"].(map[string]interface{})
+		gameData := event["post"].([]interface{})
+		pre := event["pre"].([]interface{})
+		gameData = append(gameData, pre...) // They are of same type
+
+		for _, game := range gameData {
+			gm := game.(map[string]interface{})
+
+			// Check if done
+			if !(gm["status"].(map[string]interface{})["completed"].(bool)) {
+				continue
+			}
+			if gm["customText"] == "BYE WEEK" {
+				continue
+			}
+
+			// Get data
+			opponent := gm["opponent"].(map[string]interface{})["links"].(string)
+			result := gm["result"].(map[string]interface{})
+			scoreV := result["currentTeamScore"].(string)
+			opponentScoreV := result["opponentTeamScore"].(string)
+
+			// Parse data
+			score, err := strconv.Atoi(scoreV)
+			if err != nil {
+				panic(err)
+			}
+			opponentScore, err := strconv.Atoi(opponentScoreV)
+			if err != nil {
+				panic(err)
+			}
+			opponent = strings.TrimPrefix(opponent, "/nfl/team/_/name/")
+
+			games = append(games, Game{
+				Score:         score,
+				OpponentScore: opponentScore,
+				Opponent:      opponent,
+			})
+		}
+	}
+
+	// Make team
+	tm = Team{
+		Name:  name,
+		Games: games,
+	}
 
 	// Persist
 	teams[name] = tm
--- main.go
+++ main.go
@@ -4,4 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println(GetTeamScore("lar/los_angeles_rams", 0))
+	fmt.Println(GetTeamScore("cin/cincinnati-bengals", 0))
+	dataFile.Close()
 }
