344ac1a49ff15ac9240cbcd6a41d8f1bda61155b
Nv7 <junk4Nv7@gmail.com>
2022-02-13 10:40:17 -0800

    Switch over to floats and get conclusions

--- calculate.go
+++ calculate.go
@@ -1,23 +1,44 @@
 package main
 
-func GetTeamScore(teamName string, depth int) int {
+func GetTeamScore(teamName string, depth int, noclamp ...bool) float64 {
+	if depth < 0 {
+		return 1
+	}
+
 	team := GetTeam(teamName)
-	scores := make([]int, len(team.Games))
+
+	// Get oppScores
+	oppScores := make([]float64, len(team.Games))
 	for i, game := range team.Games {
-		if depth <= 0 {
-			scores[i] = game.Score - game.OpponentScore
-		} else {
-			scores[i] = (game.Score - game.OpponentScore) * GetTeamScore(game.Opponent, depth-1)
+		oppScores[i] = GetTeamScore(game.Opponent, depth-1)
+	}
+	max := float64(-1)
+	for _, score := range oppScores {
+		if score > max {
+			max = score
+		}
+	}
+	if depth == 0 {
+		max++ // Make loses worth the loss when doing average diff (otherwise max - oppScores[i] would just be 0)
+	}
+
+	scores := make([]float64, len(team.Games))
+	for i, game := range team.Games {
+		diff := float64(game.Score - game.OpponentScore)
+		if diff > 0 { // Win, if you beat a good team it should add more
+			scores[i] = diff * oppScores[i]
+		} else { // Lose, if you lost to a bad team it should subtract more
+			scores[i] = diff * (max - oppScores[i])
 		}
 	}
 
-	average := 0
+	average := float64(0)
 	for _, score := range scores {
 		average += score
 	}
-	average /= len(scores)
+	average /= float64(len(scores))
 
-	if average < 1 { // Clamp to 1 so that when used recursively it doesn't hurt the team to play a bad team
+	if average < 1 && len(noclamp) == 0 { // Clamp to 1 so that when used recursively it doesn't hurt the team to play a bad team
 		return 1
 	}
 	return average
--- main.go
+++ main.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(GetTeamScore("lar/los_angeles_rams", 0))
-	fmt.Println(GetTeamScore("cin/cincinnati-bengals", 0))
+	fmt.Println(GetTeamScore("lar/los_angeles_rams", 2, true))
+	fmt.Println(GetTeamScore("cin/cincinnati-bengals", 2, true))
+
+	/*
+		Notes:
+			- When losing is weighted same as winning (losing against bad team is weighted less that losing against good team), bengals win
+			- When losing against bad team is weighted more, then rams win
+			- Increasing recursion depth just makes rams win more
+			- When losing against bad team is weighted less, then rams win more when recursion depth increases
+			- Rams has average win score of 4.5, Bengals have average win score of 4.1, these teams are really close
+	*/
 	dataFile.Close()
 }
